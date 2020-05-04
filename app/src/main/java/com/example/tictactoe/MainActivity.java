88d85008003846d42cf26c977e41826a46a775f5
David Rodriguez <davidrodriguezjenkins@gmail.com>
2020-05-04 13:10:30 -0700

    Added OnClick Button Array

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.tictactoe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    //Make a field for button Array
+    private Button[] buttonAry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Add method to onCreate
+        initializeButtons();
+
+    }
+
+
+    // Method creates an array of buttons
+    private void initializeButtons(){
+
+        buttonAry = new Button[]
+        {
+            findViewById(R.id.button),
+            findViewById(R.id.button2),
+            findViewById(R.id.button3),
+            findViewById(R.id.button4),
+            findViewById(R.id.button5),
+            findViewById(R.id.button6),
+            findViewById(R.id.button7),
+            findViewById(R.id.button8),
+            findViewById(R.id.button9)
+        };
+
+
     }
 }
